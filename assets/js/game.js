52eb161270a69729743a31ff9af3573ed18a0a75
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-06-07 15:02:54 -0500

    Javascript test

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+// This creates a function named "Fight"
+
+var playerName = window.prompt("What is your robots name?");
+
+console.log(playerName);
+
+console.log("This log starts a string, good for leaving yourself a message.")
+
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+// fight();
