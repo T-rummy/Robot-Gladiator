857e718dafaa29612027f5fe6c58e9ecba99719d
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-06-14 17:28:40 -0500

    streamlined shop options

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,22 +151,21 @@ var endGame = function() {
 
 var shop = function() {
     
-    var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice.");
-
+    var shopOptionPrompt = window.prompt("Press (1) to REFILL your health, (2) UPGRADE your attack, or (3) LEAVE the store? Please enter one: '1' , '2' , or '3' to make a choice.");
+    
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+    
     switch(shopOptionPrompt) {
 
-        case "REFILL":
-        case "refill":
+        case 1:
           playerInfo.refillHealth();
             break;
 
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
         
-        case "LEAVE":
-        case "leave":
+        case 3:
             window.alert("Leaving the store");
             break;
 
