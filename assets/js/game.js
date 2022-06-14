3b59d60a0722acc5ec490ecf98e437b80cc5eeb3
Ruminert <ruminertt@tanners-air.attlocal.net>
2022-06-14 17:52:04 -0500

    Randomize statrting order

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,30 +28,39 @@ var fightOrSkip = function () {
 
 var fight = function (enemy) {
 
+    var isPlayerTurn = true;
+
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    };
+
 
 
 
          while (enemy.health > 0 && playerInfo.health > 0) {
 
-           if  (fightOrSkip()) {
+           if  (isPlayerTurn) {
+               if (fightOrSkip()){
                break;
+               
            }
 
 
-                //Subtract the value of `playerInfo.attack` from the value of `enemy.health` and use that result to update the value in the `enemy.health` variable
                 var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack)
 
                  enemy.health = Math.max(0, enemy.health - damage);
   
-                 // Log a resulting message to the console so we know that it worked.
+                 
 
                 console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
 
-                 //check enemy health
+                 
   
 
                 if (enemy.health <= 0) {
                  window.alert(enemy.name + " has died!");
+
+                 playerInfo.money += 20;
 
                  break;
                     
@@ -61,9 +70,9 @@ var fight = function (enemy) {
                 
                     window.alert(enemy.name + " still has " + enemy.health + " health left.");
 
-                    }
+                    } 
         
-                    // Subtract the value of `enemy.attack` from the value of `playerInfo.health` and use that result to update the value in the `playerInfo.health` variable.
+                    
 
                         var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
@@ -88,9 +97,14 @@ var fight = function (enemy) {
                         }
 
                     }
+               
                 
+                isPlayerTurn = !isPlayerTurn;
 
-        };
+        }
+    };
+
+
 
 
 
